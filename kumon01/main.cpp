33567274ef5fd9c6550469e4b2c6c99d82276ae7
DESKTOP-9FM1A4D\vantan <mjoni3899@gmail.com>
2024-11-16 13:30:51 +0900

    公文式課題提出

--- kumon01/main.cpp
+++ kumon01/main.cpp
@@ -6,9 +6,22 @@ int main(int argc, char* argv[])
 	{
 		//判定処理をここに書いてください
 		 
-		//出力は以下の形式を使用してください
-		//std::cout << i << std::endl;		//数字を出力する
-		//std::cout << "hoge" << std::endl;	//文字列を出力する
+		int n;
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				std::cout << "hogepiyo" << std::endl;
+			}
+			else if (i % 3 == 0) {
+				std::cout << "hoge" << std::endl;
+			}
+			else if (i % 5 == 0) {
+				std::cout << "piyo" << std::endl;
+			}
+			else
+			{
+				std::cout << i << std::endl;
+			}
+		}
 	}
 
     return 0;
